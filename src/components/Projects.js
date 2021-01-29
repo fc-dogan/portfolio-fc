fbe72d3350e9348cf945f0b36488e154bcd342cf
Fatma <fceyda@gmail.com>
2021-01-28 17:05:37 -0800

    update projectsData

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,86 @@
-import React from 'react'
+import React from 'react';
+import childtrack from '../assets/chilTrack.png';
+import pierres from '../assets/pierres.png';
+import airport from '../assets/PDXAirportGuide.png';
+import taproom from '../assets/taproom.png';
+import hangman from '../assets/hangman.png';
+import { Card, Button, Row, Col, Container } from 'react-bootstrap';
+
+const projectsData = [
+  {
+    name: 'ChilTrack',
+    description: 'An application for parents who can track their kids’ behavior, daily progress, and set a goal(reward) to motivate them',
+    image: childtrack,
+    sourceUrl: 'https://github.com/fc-dogan/ChilTrack',
+    liveSite: 'https://childtrack-b1767.web.app/',
+  },
+  {
+    name: "Pierre's Sweet and Savory Treats",
+    description: 'An ASP.NET MVC application with user authentication and many-to-many database relationship.',
+    image: pierres,
+    sourceUrl: 'https://github.com/fc-dogan/Pierre-s-Treats',
+    liveSite: '',
+  },
+  {
+    name: 'Animal Shelter API',
+    description: 'A REST API with ASP.NET for a local animal shelter',
+    image: '',
+    sourceUrl: 'https://github.com/fc-dogan/AnimalShelterAPI',
+    liveSite: '',
+  },
+  {
+    name: 'Taproom',
+    description: `An application built in React Redux to track a taproom's taps by name, brand, price, flavor, and quantity.`,
+    image: taproom,
+    sourceUrl: 'https://github.com/fc-dogan/Taproom-Redux',
+    liveSite: '',
+  },
+  {
+    name: 'Hangman',
+    description: `Hangman game built using React, Redux, Node.js, Bootstrap `,
+    image: hangman,
+    sourceUrl: 'https://github.com/fc-dogan/hangman',
+    liveSite: 'https://fc-dogan.github.io/hangman/',
+  },
+  {
+    name: 'Pdx Airport Guide',
+    description: 'Website application for travelers where they can find airport amenities and flight information.',
+    image: airport,
+    sourceUrl: 'https://github.com/fc-dogan/pdx-airport-guide',
+    liveSite: '',
+  },
+]
 
 function Projects() {
+
+  
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <React.Fragment>
+      <Container>
+        <h1>Projects</h1>
+        <Row>
+          {projectsData.map( (project , index )=> {
+            return (
+              <Col key={index} >
+              <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={project.image} style={{ width: '10rem', alignSelf: 'center' }}/>
+              <Card.Body>
+                <Card.Title>{project.name}</Card.Title>
+                <Card.Text>
+                  {project.description}
+                </Card.Text>
+                <Card.Link href={project.liveSite}>Source</Card.Link>
+                <Card.Link href={project.liveSite}>{project.liveSite ? 'View Live' : ''}</Card.Link>
+              </Card.Body>
+              </Card>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+      
+      
+    </React.Fragment>
   )
 }
 
